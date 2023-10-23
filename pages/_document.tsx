6d5b091f3bd21da5fdaed199a1e92124bfe9982c
Elijah <elijah@nameless.io>
2023-10-23 13:20:57 -0400

    chore: update meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -44,14 +44,14 @@ class MyDocument extends Document {
           <link rel="icon" sizes="16x16" type="image/png"href="/static/favicon-16x16.png"/> */ }
           <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png"/>
           <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#5bbad5"/>
-          <meta property="og:title" content="Blockscout: A block explorer designed for a decentralized world."/>
+          <meta property="og:title" content="StealthTest Block Explorer"/>
           <meta
             property="og:description"
             // eslint-disable-next-line max-len
-            content="Blockscout is the #1 open-source blockchain explorer available today. 100+ chains and counting rely on Blockscout data availability, APIs, and ecosystem tools to support their networks."
+            content="The advanced block explorer solution for web3 development."
           />
           <meta property="og:image" content={ config.app.baseUrl + '/static/og.png' }/>
-          <meta property="og:site_name" content="Blockscout"/>
+          <meta property="og:site_name" content="StealthTest Block Explorer"/>
           <meta property="og:type" content="website"/>
           <meta name="twitter:card" content="summary_large_image"/>
           <meta property="twitter:image" content={ config.app.baseUrl + '/static/og_twitter.png' }/>
